7f79b9b6e538ef0ad4f3caf10af94c5a02f965c4
MicrocontrollersDev <microcontrollersyt@gmail.com>
2024-04-03 11:00:12 -0500

    1.3.3 make notice of potentially unstable features

--- src/main/java/dev/microcontrollers/autoskyblockrejoin/config/RejoinConfig.java
+++ src/main/java/dev/microcontrollers/autoskyblockrejoin/config/RejoinConfig.java
@@ -1,13 +1,16 @@
 package dev.microcontrollers.autoskyblockrejoin.config;
 
 import cc.polyfrost.oneconfig.config.Config;
-import cc.polyfrost.oneconfig.config.annotations.DualOption;
-import cc.polyfrost.oneconfig.config.annotations.Info;
-import cc.polyfrost.oneconfig.config.annotations.Slider;
-import cc.polyfrost.oneconfig.config.annotations.Switch;
+import cc.polyfrost.oneconfig.config.annotations.*;
+import cc.polyfrost.oneconfig.config.annotations.Button;
 import cc.polyfrost.oneconfig.config.data.InfoType;
 import cc.polyfrost.oneconfig.config.data.Mod;
 import cc.polyfrost.oneconfig.config.data.ModType;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class RejoinConfig extends Config {
     @Info(
@@ -52,12 +55,50 @@ public class RejoinConfig extends Config {
     )
     public static int shiftTime = 1;
 
+    @Info(
+            text = "This feature has not been tested! Use at your own risk! Please report in the discord if it works correctly.",
+            size = 2,
+            type = InfoType.ERROR,
+            subcategory = "General"
+    )
+    private boolean error = false;
+
     @Switch(
             name = "Rejoin Hypixel on Disconnect",
-            description = "Untested, but should work in theory. Automatically attempts a full reconnect to the network and then to SkyBlock.",
+            description = "Automatically attempts a full reconnect to the network and then to SkyBlock.",
             subcategory = "General"
     )
     public static boolean serverConnect = false;
+
+    @Button(
+            name = "Join the Discord",
+            text = "Click",
+            subcategory = "Socials"
+    )
+    Runnable discord = () -> {
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            try {
+                Desktop.getDesktop().browse(new URI("https://discord.gg/rejfv9kFJj"));
+            } catch (IOException | URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    };
+
+    @Button(
+            name = "Check Out My Other Mods",
+            text = "Click",
+            subcategory = "Socials"
+    )
+    Runnable modrinth = () -> {
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            try {
+                Desktop.getDesktop().browse(new URI("https://modrinth.com/user/Microcontrollers"));
+            } catch (IOException | URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    };
 
     public RejoinConfig() {
         super(new Mod("Auto Skyblock Rejoin", ModType.HYPIXEL), "autoskyblockrejoin.json");
